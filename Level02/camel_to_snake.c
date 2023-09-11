2beafee327cea9fc45553f04a95802f68aa0f37f
regexp <vradis.florez@gmail.com>
2023-09-11 07:37:47 +0200

    More Level 2

--- Level02/camel_to_snake.c
+++ Level02/camel_to_snake.c
@@ -27,3 +27,27 @@ and returns 0. Then it loops through the string and checks if the character
 is in upper case. If it is, it writes an underscore and changes the character 
 to lower case. It then writes the character to the output. Finally, it writes 
 a newline and returns 0.*/
+
+#include <unistd.h>
+
+int main(int argc , char *argv[])
+{
+    char *str = argv[1];
+    int i = 0;
+    
+    if (argc == 2)
+    {
+        while( str[i] != '\0')
+        {
+            if (str[i] >= 'A' && str[i] <= 'Z')
+            {
+                write(1, "_", 1);
+                str[i] += 32;
+            }
+            write(1, &str[i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return 0;
+}
