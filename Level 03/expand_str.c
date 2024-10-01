725388a36a773b2a6220c3ce6bb4605916cd1aaa
Vradis <105650269+Vradss@users.noreply.github.com>
2024-10-01 18:47:17 +0200

    Update expand_str.c

--- Level 03/expand_str.c
+++ Level 03/expand_str.c
@@ -54,3 +54,38 @@ int main(int argc , char *argv[])
     write(1, "\n", 1);
     return (0);
 }
+
+
+///
+
+int main (int argc, char *argv [])
+{
+    char *str = argv[1];
+    int i =  0;
+    int first_word = 1;
+    
+    
+    if (argc == 2)
+    {
+        while (str[i] == ' ' || str[i] == '\t')
+            i++;
+        while (str[i] != '\0')
+        {
+            if (str[i] == ' ' || str[i] == '\t') 
+            {
+                while (str[i] == ' ' || str[i] == '\t')
+                    i++;
+                if (str[i] && !first_word)
+                    write(1, "   ", 3);
+            }
+            if (str[i] && (str[i] != ' ' || str[i] != '\t'))
+            {
+                write(1, &str[i], 1);
+                first_word = 0;
+                i++;
+            }
+        }
+    }
+    write(1, "\n", 1);
+    return 0; 
+}
