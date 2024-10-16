6667b0f36a319dd6b6797f41b9b23b275cf3277e
Vradis <105650269+Vradss@users.noreply.github.com>
2024-10-17 00:08:51 +0200

    Update ft_strspn.c

--- Level 02/ft_strspn.c
+++ Level 02/ft_strspn.c
@@ -10,27 +10,31 @@ Reproduce exactly the behavior of the strspn function
 The function should be prototyped as follows:
 
 size_t	ft_strspn(const char *s, const char *accept);
+
+La función strspn cuenta el número de caracteres en el inicio de la cadena s que coinciden 
+con los caracteres de la cadena accept. En otras palabras, comienza desde el principio de s 
+y cuenta cuántos caracteres son también parte de accept hasta que encuentra un carácter que no está en accept. 
+En ese momento, deja de contar y devuelve el total de caracteres que coincidieron.
+
 */
-
-#include <unistd.h>
-#include <stdio.h>
+#include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 
-size_t ft_strspn(const char *s, const char *accept)
+size_t	ft_strspn(const char *s, const char *accept)
 {
-	size_t len; //almacenara lo que cumpla las restricciones
-	int i; //recorre accept
-
-	while(s[len] != '\0')
-	{
-		i = 0;
-		while(accept[i] && accept[i] != s[len])
-			i++;
-		if(!accept[i])
-			break;
-		len++;
-	}
-	return(len);
+    int i = 0; //itera sobre accept
+    int num_coin = 0; //almacena el numero de coincidencias
+    
+    while(s[num_coin])
+    {
+        while( accept[i] && accept[i] != s[num_coin])
+            i++;
+        if(!accept[i]) //si no se encontro ninguna coincidencia
+            break; //salimos del bucle
+        num_coin++;
+    }
+    return num_coin;
 }
 
 int main()
@@ -41,6 +45,7 @@ int main()
 	printf("mia: s1 = %s, s2 = %s, result = %lu\n", s1 , s2 , ft_strspn(s1,s2));
 	printf("original : s1 = %s, s2 = %s, result = %lu\n", s1, s2 , strspn(s1,s2));
 	printf("original : s1 = %s, s2 = %s, result = %lu\n", s1, s2 , strcspn(s1,s2));
-
 	return 0;
 }
+
+output : 3 
