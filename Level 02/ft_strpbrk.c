ece8a08b5d86d20f182c486fc3b8765b7e8bdbbd
Vradis <105650269+Vradss@users.noreply.github.com>
2024-10-16 23:11:37 +0200

    Update ft_strpbrk.c

--- Level 02/ft_strpbrk.c
+++ Level 02/ft_strpbrk.c
@@ -12,33 +12,46 @@ The function should be prototyped as follows:
 char	*ft_strpbrk(const char *s1, const char *s2);
 */
 
+
+// Su propósito principal es buscar en una cadena (string) la primera ocurrencia de 
+// cualquiera de un conjunto de caracteres especificado.
+// @Parámetros
+// s1: Es un puntero a la cadena de entrada en la que se desea buscar.
+// s2: Es un puntero a la cadena que contiene los caracteres a buscar.
+// @Retorno
+// Devuelve un puntero a la primera ocurrencia de cualquiera de los caracteres de s2 en s1.
+// Si no se encuentra ningún carácter de s2 en s1, la función devuelve NULL.
+
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
-char    *ft_strpbrk(const char *s1, const char *s2)
+char	*ft_strpbrk(const char *s1, const char *s2)
 {
-	while(*s1)
-	{
-		const char *temp = s2; //se crea un puntero que recorra s2
-		{
-			while(*temp) //creamos el bucle que recorre s2 
-			{		
-				if(*s1 == *temp)
-					return (char *)s1;
-				temp++;
-			}
-		}
-		s1++;
-	}
-	return NULL;
+    while (*s1)
+    {
+        const char *temp = s2; //creamos un puntero temporal para recorrrer s2
+        while (*temp)
+        {
+            if ( *s1 == *temp) 
+                return (char *)s1; //retornamos puntero desde la posición donde hay coincidencia
+            temp++;
+        }
+        s1++;
+    }
+    return(NULL);
 }
+
 
 
 int main()
 {
 	char *s1 = "vradis";
-	char *s2 = "sabrina";
+	char *s2 = "eoa";
 	printf("mia : %s\n", ft_strpbrk(s1,s2));
 	printf("original : %s", strpbrk(s1,s2));
 	return 0;
 }
+
+//ubica la letra en s1 y devuelve string desde ese punto
+output : adis
