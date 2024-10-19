0dfb49499dc4c829b33af49d4d73c48a3c8af68e
Vradis <105650269+Vradss@users.noreply.github.com>
2024-10-19 12:31:39 +0200

    subject

--- Level 01/firstword.c
+++ Level 01/firstword.c
@@ -1,20 +1,48 @@
-#include <stdio.h>
+// Assignment name  : first_word
+// Expected files   : first_word.c
+// Allowed functions: write
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a string and displays its first word, followed by a
+// newline.
+
+// A word is a section of string delimited by spaces/tabs or by the start/end of
+// the string.
+
+// If the number of parameters is not 1, or if there are no words, simply display
+// a newline.
+
+// Examples:
+
+// $> ./first_word "FOR PONY" | cat -e
+// FOR$
+// $> ./first_word "this        ...    is sparta, then again, maybe    not" | cat -e
+// this$
+// $> ./first_word "   " | cat -e
+// $
+// $> ./first_word "a" "b" | cat -e
+// $
+// $> ./first_word "  lorem,ipsum  " | cat -e
+// lorem,ipsum$
+// $>
+
 #include <unistd.h>
 
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
-    int i;
     char *str = argv[1];
-
-    i = 0;
+    int i = 0;
 
     if (argc == 2)
     {
-        while (str[i] == ' ') //saltamos los espacios en blanco
+        while(str[i] == ' ' || str[i] == '\t' ) //saltamos space & tabs
             i++;
-        while (str[i] != ' ' && str[i] != '\0') // posicion no es blanco y tmpoco nulo ( fin cadena)
-            write (1, &str[i++], 1);
+        while(str[i] != ' ' && str[i] != '\t' )
+        {
+            write(1, &str[i], 1);
+            i++;
+        }
     }
-    write(1,"\n", 1);
+    write(1, "\n", 1);
     return 0;
 }
