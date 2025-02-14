d07274b1cf70f760e22524ac706034e6df538cb5
Vradis <105650269+Vradss@users.noreply.github.com>
2025-02-14 21:24:19 +0100

    Update do_op.c

--- Level 02/do_op.c
+++ Level 02/do_op.c
@@ -59,3 +59,27 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+
+int main(int argc, char *argv[])
+{
+
+    if ( argc == 4 )
+    {
+        int n1 = atoi(argv[1]);
+        char op = argv[2][0];
+        int n2 = atoi(argv[3]);
+
+        if ( op == '*')
+            printf("%d", n1 * n2);
+        else if (op == '/')
+            printf("%d", n1 / n2);
+        else if (op == '+')
+            printf("%d", n1 + n2);
+        else if (op == '%')
+            printf("%d", n1 % n2);
+        else if (op == '-')
+            printf("%d", n1 - n2);
+    }
+    printf("\n");
+    return 0;
+}
