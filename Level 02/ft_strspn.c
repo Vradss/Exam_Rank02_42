f1e34f1e146237b4799eb97cba890f96009e6e05
Vradis <105650269+Vradss@users.noreply.github.com>
2025-02-15 14:02:50 +0100

    Update ft_strspn.c

--- Level 02/ft_strspn.c
+++ Level 02/ft_strspn.c
@@ -49,3 +49,40 @@ int main()
 }
 
 output : 3 
+
+// new
+
+#include <stddef.h>
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+    size_t i = 0;
+    size_t j ;
+
+    while(s[i])
+    {
+        j = 0;
+        while(accept[j])
+        {
+            if ( s[i] == accept[j])
+                break;
+            j++;
+        }
+        if (!accept[j])
+            return i;
+        i++;
+    }
+    return i;
+}
+
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    char *s1 = argv[1];
+    char *accept = argv[2];
+    
+    printf("%zu\n", ft_strspn(s1, accept));
+    printf("%lu\n", strspn(s1, accept));
+}
