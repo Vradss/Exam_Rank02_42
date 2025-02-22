0b5f136578ff464ccdb8b89ffae4eece2d2fed68
Vradis <105650269+Vradss@users.noreply.github.com>
2025-02-22 10:48:38 +0100

    Update fprime.c

--- Level 04/fprime.c
+++ Level 04/fprime.c
@@ -38,29 +38,27 @@ $*/
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int num;
+    int i = 2;
 
-	if (argc == 2)
-	{
-		i = 2; //first prime number
-		num = atoi(argv[1]); //convert str to int
-		if (num == 1)
-			printf("1");
-		while (num >= i)
-		{
-			//check if its divisible by i
-			if (num % i == 0)
-			{
-				printf("%d", i);
-				if(num != i)
-					printf("*");
-				//update the num to be result of the division
-				num = num / i;
-				i = 1;
-			}
-			i++;
-		}
-	}
-	printf("\n");
+    if ( argc == 2)
+    {
+        int num = atoi(argv[1]);
+        if ( num == 1)
+            printf("1");
+
+        while (num > 1)
+        {
+            if( num % i == 0)
+            {
+                printf("%d", i);
+                num = num / i;
+                if ( num > 1)
+                    printf("*");        
+            }
+            else 
+                i++;
+        }
+    }
+    printf("\n");
+    return 0;
 }
